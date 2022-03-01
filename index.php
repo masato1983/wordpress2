445bacab411eee447181c80645b99924b5add7ad
masato1983 <50264750+masato1983@users.noreply.github.com>
2022-03-01 12:59:58 +0900

    feat(lecture8): ループのコーディング

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php get_header(); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php if (have_posts()) { ?>
+    <?php while (have_posts()) { ?>
+        <?php the_post(); ?>
+        <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+        <div>
+            <span>Posted on</span>
+            <a href="<?php echo get_permalink(); ?>"><time datetime="<?php echo get_the_date('c') ?>"><?php echo get_the_date(); ?></time></a>
+            <span>By</span>
+            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>"><?php echo get_the_author(); ?></a>
+        </div>
+        <div>
+            <?php the_excerpt(); ?>
+        </div>
+        <a href="<?php echo get_the_permalink() ?>" title="<?php the_title_attribute(); ?>">Read More <span class="u-screen-reader-text">About <?php the_title() ?></span></a>
+    <?php } ?>
+    <?php the_posts_pagination(); ?>
+<?php } else { ?>
+    <p>Sorry, no posts matched your criteria.</p>
+<?php } ?>
 
-<body>
-
-</body>
-
-</html>
+<?php get_footer(); ?>
