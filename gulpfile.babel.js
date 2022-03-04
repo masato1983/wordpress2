f21bb8408d32fcc9e70e7732f2e25a608d6a8693
masato1983 <50264750+masato1983@users.noreply.github.com>
2022-03-04 11:57:01 +0900

    feat(lecture18): Creating A Gulp Task For Styles cont

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,16 +4,31 @@ import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import cleanCss from 'gulp-clean-css';
 import gulpif from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
 
 const sass = gulpSass(dartSass);
 const PRODUCTION = yargs.argv.prod;
 
-export const style = () => {
-    return gulp.src('src/assets/scss/bundle.scss')
+const paths = {
+    styles: {
+        src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+        dest: 'dist/assets/css',
+        watch: 'src/assets/scss/**/*.scss'
+    }
+}
+
+export const styles = () => {
+    return gulp.src(paths.styles.src)
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
         .pipe(sass().on('error', sass.logError))
         .pipe(gulpif(PRODUCTION, cleanCss({debug: true}, (details) => {
             console.log(`${details.name}: originalSize ${details.stats.originalSize}`);
             console.log(`${details.name}: minifiedSize ${details.stats.minifiedSize}`);
         })))
-        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(gulp.dest(paths.styles.dest))
+}
+
+export const watch = () => {
+    gulp.watch(paths.styles.watch, styles)
 }
--- library/enqueue-assets.php
+++ library/enqueue-assets.php
@@ -1,8 +1,17 @@
 <?php
 
+// theme style
 function udemyWordpress2_assets()
 {
     wp_enqueue_style('udemy_worpress2-stylesheet', get_template_directory_uri() . '/dist/assets/css/bundle.css', array(), '1.0.0', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'udemyWordpress2_assets');
+
+// admin style
+function udemyWordpress2_admin_assets()
+{
+    wp_enqueue_style('udemy_worpress2-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), '1.0.0', 'all');
+}
+
+add_action('admin_enqueue_scripts', 'udemyWordpress2_admin_assets');
