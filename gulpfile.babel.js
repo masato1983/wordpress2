37df6d5266526ee033810d1d6ebfefde82283034
masato1983 <50264750+masato1983@users.noreply.github.com>
2022-03-05 09:15:59 +0900

    feat(lecture23): Creating A Gulp Task For JavaScript cont

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,9 @@ import gulpif from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
 import imagemin from 'gulp-imagemin';
 import del from 'del';
+import webpack from 'webpack-stream';
+import uglify from 'gulp-uglify';
+import named from 'vinyl-named';
 
 const sass = gulpSass(dartSass);
 const PRODUCTION = yargs.argv.prod;
@@ -15,6 +18,10 @@ const paths = {
     styles: {
         src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
         dest: 'dist/assets/css',
+    },
+    scripts: {
+        src: ['src/assets/js/bundle.js', 'src/assets/js/admin.js'],
+        dest: 'dist/assets/js'
     },
     images: {
         src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif,webp}',
@@ -26,10 +33,10 @@ const paths = {
     }
 }
 
-// Clean task
+// clean
 export const clean = () => del('dist');
 
-// Styles task
+// styles
 export const styles = () => {
     return gulp.src(paths.styles.src)
         .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
@@ -42,29 +49,58 @@ export const styles = () => {
         .pipe(gulp.dest(paths.styles.dest));
 }
 
-// Images task
+// scripts
+export const scripts = () => {
+    return gulp.src(paths.scripts.src)  
+        .pipe(named()) 
+        .pipe(webpack({
+            mode: !PRODUCTION ? 'development' : 'production',
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['@babel/preset-env']
+                            }
+                        }
+                    }
+                ]
+            },
+            output: {
+                filename: '[name].js'
+            },
+            devtool: !PRODUCTION ? 'inline-source-map' : false
+        }))
+        .pipe(gulpif(PRODUCTION, uglify()))
+        .pipe(gulp.dest(paths.scripts.dest));
+}
+
+// images
 export const images = () => {
     return gulp.src(paths.images.src)
         .pipe(gulpif(PRODUCTION, imagemin()))
         .pipe(gulp.dest(paths.images.dest));
 }
 
-// Watch task
+// watch
 export const watch = () => {
     gulp.watch('src/assets/scss/**/*.scss', styles);
+    gulp.watch('src/assets/js/**/*.js', scripts);
     gulp.watch(paths.images.src, images);
     gulp.watch(paths.other.src, copy);
 }
 
-// Copy task
+// copy
 export const copy = () => {
     return gulp.src(paths.other.src)
         .pipe(gulp.dest(paths.other.dest));
 }
 
-// Build Task
-export const dev = gulp.series(clean, gulp.parallel(styles, images, copy), watch);
-export const build = gulp.series(clean, gulp.parallel(styles, images, copy));
+// dev, build
+export const dev = gulp.series(clean, gulp.parallel(styles, scripts, images, copy), watch);
+export const build = gulp.series(clean, gulp.parallel(styles, scripts, images, copy));
 
-// Default Task
+// default
 export default dev;
--- library/enqueue-assets.php
+++ library/enqueue-assets.php
@@ -4,6 +4,7 @@
 function udemyWordpress2_assets()
 {
     wp_enqueue_style('udemy_worpress2-stylesheet', get_template_directory_uri() . '/dist/assets/css/bundle.css', array(), '1.0.0', 'all');
+    wp_enqueue_script('udemy_wordpress2-scripts', get_template_directory_uri() . '/dist/assets/js/bundle.js', array(), '1.0.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'udemyWordpress2_assets');
@@ -12,6 +13,7 @@ add_action('wp_enqueue_scripts', 'udemyWordpress2_assets');
 function udemyWordpress2_admin_assets()
 {
     wp_enqueue_style('udemy_worpress2-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), '1.0.0', 'all');
+    wp_enqueue_script('udemy_wordpress2-admin-scripts', get_template_directory_uri() . '/dist/assets/js/admin.js', array(), '1.0.0', true);
 }
 
 add_action('admin_enqueue_scripts', 'udemyWordpress2_admin_assets');
